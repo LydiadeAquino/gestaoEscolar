1420656317ff8216470d3744a40228ae42016d71
Lydia de Aquino <lydiaaquino83@gmail.com>
2026-08-21 14:50:30 -0300

    primeiro commit: estrutura inicial do Spring Boot

---
+++ GestaoEscolar/src/test/java/LK/GestaoEscolar/GestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package LK.GestaoEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
